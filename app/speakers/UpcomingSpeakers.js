cd42745ccf9c2b40aeff15e23f321d47b09101cd
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2025-01-11 13:50:06 +0530

    Update UpcomingSpeakers.js

--- app/speakers/UpcomingSpeakers.js
+++ app/speakers/UpcomingSpeakers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaLinkedin, FaInstagram, FaFilter } from 'react-icons/fa';
+import { FaLinkedin, FaInstagram} from 'react-icons/fa';
 import { AiOutlineInstagram, AiOutlineLinkedin } from 'react-icons/ai';
 import MarqueeComponent from './MarqueeComponent';
 import Image from 'next/image';
